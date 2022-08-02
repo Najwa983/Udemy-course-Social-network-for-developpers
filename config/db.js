c3bed11f3684698f07b40b5404bcf45d3018f6e8
Najwa983 <Najwa.lakmouri1@gmail.com>
2022-08-02 10:50:48 +0100

    User api routes and json web token (JWT) authentication

--- config/db.js
+++ config/db.js
@@ -3,16 +3,15 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(db);
+  try {
+    await mongoose.connect(db, {});
 
-        console.log('MongoDB Conected...')
-    } catch(err){
-        console.log(err.message);
-        // exit process with failure
-        process.exit(1);
-
-    }
-}
+    console.log('MongoDB Conected...');
+  } catch (err) {
+    console.log(err.message);
+    // exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
